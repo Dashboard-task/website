f321302fd94b54bb6302ce091cf68051ac66c378
Rocknologie <rocknologie@hotmail.fr>
2020-10-04 17:38:53 +0200

    Ouai

--- src/navigation/route.tsx
+++ src/navigation/route.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Col, Layout, Row } from 'antd';
 import { NavBar } from '../shared/components/navbar';
-import { TaskComponent } from '../features/task/tast';
-import { TopicComponent } from '../features/topic/topic';
 
 const { Header, Footer, Sider, Content } = Layout;
 /*
@@ -20,24 +18,24 @@ export const MainRouter: React.FC = () => (
 
                 <Row justify="center">
                     <Layout>
-                        <Col span={3} style={{backgroundColor: 'red'}}>
+                        <Col span={3} style={{ backgroundColor: 'red' }}>
                             <Sider>
                                 <Route path="/">
-                                    <TopicComponent />
+
                                 </Route>
                             </Sider>
                         </Col>
-                        <Col span={18} style={{backgroundColor: 'blue'}}>
+                        <Col span={18} style={{ backgroundColor: 'blue' }}>
                             <Content>
                                 <Route path="/">
-                                    <TaskComponent />
+
                                 </Route>
                             </Content>
                         </Col>
-                        <Col span={3} style={{backgroundColor: 'green'}}>
+                        <Col span={3} style={{ backgroundColor: 'green' }}>
                             <Sider>
                                 <Route path="/">
-                                    <a>tutu</a>
+
                                 </Route>
                             </Sider>
                         </Col>
