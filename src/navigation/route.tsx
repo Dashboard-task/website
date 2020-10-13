df3df4f1616c4dc06ce1846e6ad8b222e73237c1
Rocknologie <rocknologie@hotmail.fr>
2020-10-13 21:58:53 +0200

    Update components route

--- src/navigation/route.tsx
+++ src/navigation/route.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { Col, Layout, Row } from 'antd';
-import { NavBar } from '../shared/components/navbar';
-import { TaskComponent } from '../features/task/tast';
-import { TopicComponent } from '../features/topic/topic';
+import { HomePage } from '../components/home-page/home-page';
 
-const { Header, Footer, Sider, Content } = Layout;
 /*
 * Routing page:
 TODO Faire la distribution des routes
@@ -13,43 +9,9 @@ TODO Faire la distribution des routes
 export const MainRouter: React.FC = () => (
     <BrowserRouter>
         <Switch>
-            <Layout>
-                <Route path='/'>
-                    <NavBar />
-                </Route>
-
-                <Row justify="center">
-                    <Layout>
-                        <Col span={3} style={{backgroundColor: 'red'}}>
-                            <Sider>
-                                <Route path="/">
-                                    <TopicComponent />
-                                </Route>
-                            </Sider>
-                        </Col>
-                        <Col span={18} style={{backgroundColor: 'blue'}}>
-                            <Content>
-                                <Route path="/">
-                                    <TaskComponent />
-                                </Route>
-                            </Content>
-                        </Col>
-                        <Col span={3} style={{backgroundColor: 'green'}}>
-                            <Sider>
-                                <Route path="/">
-                                    <a>tutu</a>
-                                </Route>
-                            </Sider>
-                        </Col>
-                    </Layout>
-                </Row>
-
-                <Route path='/'>
-                    <Footer>
-                        {/* <FooterPage /> */}
-                    </Footer>
-                </Route>
-            </Layout>
+            <Route path='/'>
+                <HomePage />
+            </Route>
         </Switch>
     </BrowserRouter>
 )
